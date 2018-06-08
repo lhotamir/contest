163d2cd5f8713e12de05990bdd97f5a36f1b2aeb
Anna Lhoťanová <anna.lhotanova@post.cz>
2018-06-08 13:40:57 +0200

    Nastylování okna pro soutěžící.

--- contest.spa/index.js
+++ contest.spa/index.js
@@ -5,14 +5,17 @@ function doAjax() {
             url: 'http://localhost:5000/current',
             success: function (data) {
                 $('#question').text(data.question);
-                var list = $('<ul></ul>')
-                $('#answers').empty()
+                var list = $('<ul></ul>');
+                $('#answers').empty();
                 $.each(data.answers, function (i) {
                     var li = $('<li></li>')
                         .text(data.answers[i].answer)
                         .appendTo(list);
+					var span = $('<span></span>');
+					span.text(data.answers[i].points);
+					span.appendTo(li);
                 });
-                $('#answers').append(list)
+                $('#answers').append(list);
             }
         }
     );
@@ -21,7 +24,7 @@ function doAjax() {
             type: 'GET',
             url: 'http://localhost:5000/score',
             success: function (data) {
-                $('#score').text("Skóre: "+data.score)
+                $('#score').text(data.score);
             }
         }
     );
@@ -31,18 +34,18 @@ function doAjax() {
             url: 'http://localhost:5000/strikes',
             success: function (data) {
                 var strikes_div = $('#strikes');
-                strikes_div.empty()
+                strikes_div.empty();
                 var strikes_p = $('<p></p>');
                 var s = '';
                 for (let i = 0; i < data.strikes; i++) {
                     s += 'X ';
                 }
-                strikes_p.text(s)
-                strikes_p.appendTo(strikes_div)
+                strikes_p.text(s);
+                strikes_p.appendTo(strikes_div);
             },
             complete: function (data) {
                 setTimeout(doAjax, 1000);
             }
         }
-    )
+    );
 }
