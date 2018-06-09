5bbf69565bf21c966bee1a7c441e4537aa1a0850
Miroslav Lhoťan <miroslav@lhotan.cz>
2018-06-09 09:59:09 +0200

    added points multiplying

--- contest.api/contest.py
+++ contest.api/contest.py
@@ -3,6 +3,8 @@ import json
 
 class Contest(object):
 
+    SCORE_MULTIPLIER = {5:2, 4:3}
+
     def __init__(self):
         with open("contest.api/questions.json", encoding='utf-8') as f:
             data = json.load(f)
@@ -27,8 +29,15 @@ class Contest(object):
     def get_current_question(self):
         return self.questions[self.current_question_idx]
 
-    def reveal_answer(self, answer_num):
+    def reveal_answer(self, answer_num, count_score=True):
         self.current_question["answers"][answer_num] = self.questions[self.current_question_idx]["answers"][answer_num]
+        if count_score:
+            question_score = self.questions[self.current_question_idx]["answers"][answer_num]["points"]
+            try:
+                question_score *= self.SCORE_MULTIPLIER[len(self.questions[self.current_question_idx]["answers"])]
+            except KeyError:
+                pass
+            self.score += question_score
         return
 
     def final_reveal(self, answer_num):
